10bf0efc1b91faad2c1e8b9ab4c872966c20b00f
QinZheng <503731351@qq.com>
2016-04-01 01:28:05 +0800

    EZ UI setup for the login screen.

--- app/src/main/java/com/dv/persistnote/business/LoginScreen.java
+++ app/src/main/java/com/dv/persistnote/business/LoginScreen.java
@@ -5,13 +5,14 @@ import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.LinearLayout;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.dv.persistnote.R;
 import com.dv.persistnote.base.ResTools;
 import com.dv.persistnote.framework.DefaultScreen;
+import com.dv.persistnote.framework.ui.CircleView;
 import com.dv.persistnote.framework.ui.UICallBacks;
 
 import java.lang.reflect.Field;
@@ -21,11 +22,13 @@ import java.lang.reflect.Field;
  */
 public class LoginScreen extends DefaultScreen implements View.OnClickListener {
 
-    private LinearLayout mContainer;
+    private RelativeLayout mContainer;
 
     private RelativeLayout containerPhoneNumber;
 
     private RelativeLayout containerPassword;
+
+    private RelativeLayout containerOKButton;
 
     private EditText etPhoneNumber;
 
@@ -35,6 +38,10 @@ public class LoginScreen extends DefaultScreen implements View.OnClickListener {
 
     private View linePassword;
 
+    private CircleView okButton;
+
+    private ImageView okButtonArrow;
+
     public LoginScreen(Context context, UICallBacks callBacks) {
         super(context, callBacks);
         init();
@@ -42,13 +49,16 @@ public class LoginScreen extends DefaultScreen implements View.OnClickListener {
     }
 
     private void init() {
-        mContainer = new LinearLayout(getContext());
+        mContainer = new RelativeLayout(getContext());
         setContent(mContainer);
 
         mContainer.removeAllViews();
 
+        // containerPhoneNumber
+
         containerPhoneNumber = new RelativeLayout(getContext());
-        LinearLayout.LayoutParams lp1 = new LinearLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+        containerPhoneNumber.setId(R.id.login_rl_phone_number);
+        RelativeLayout.LayoutParams lp1 = new RelativeLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
         lp1.topMargin = ResTools.getDimenInt(R.dimen.login_rl_phone_number_margin_top);
         lp1.leftMargin = ResTools.getDimenInt(R.dimen.login_rl_margin_left);
         lp1.rightMargin = ResTools.getDimenInt(R.dimen.login_rl_margin_right);
@@ -96,6 +106,85 @@ public class LoginScreen extends DefaultScreen implements View.OnClickListener {
         containerPhoneNumber.addView(linePhoneNumber, lp3);
 
         mContainer.addView(containerPhoneNumber, lp1);
+
+        // containerPassword
+
+        containerPassword = new RelativeLayout(getContext());
+        containerPassword.setId(R.id.login_rl_password);
+        RelativeLayout.LayoutParams lp4 = new RelativeLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+        lp4.topMargin = ResTools.getDimenInt(R.dimen.login_rl_phone_number_margin_top);
+        lp4.leftMargin = ResTools.getDimenInt(R.dimen.login_rl_margin_left);
+        lp4.rightMargin = ResTools.getDimenInt(R.dimen.login_rl_margin_right);
+        lp4.addRule(RelativeLayout.BELOW, R.id.login_rl_phone_number);
+
+        etPassword = new EditText(getContext());
+        etPassword.setTextSize(TypedValue.COMPLEX_UNIT_PX, getResources().getDimension(R.dimen.h1));
+        etPassword.setPadding(0, ResTools.getDimenInt(R.dimen.login_et_padding_top),
+                0, ResTools.getDimenInt(R.dimen.login_et_padding_bottom));
+        etPassword.setId(R.id.login_et_password);
+        etPassword.setBackgroundColor(ResTools.getColor(R.color.c4));
+        etPassword.setHint(R.string.login_et_hint_password);
+        etPassword.setSingleLine(true);
+        etPassword.setOnFocusChangeListener(new OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (!hasFocus) {
+                    etPassword.setHint(R.string.login_et_hint_password);
+                } else {
+                    etPassword.setHint(null);
+                }
+            }
+        });
+        try {
+            // https://github.com/android/platform_frameworks_base/blob/kitkat-release/core/java/android/widget/TextView.java#L562-564
+            Field f = TextView.class.getDeclaredField("mCursorDrawableRes");
+            f.setAccessible(true);
+            f.set(etPassword, R.drawable.color_cursor);
+        } catch (Exception ignored) {
+            Log.e(TAG, "Login Init : color_cursor Error");
+        }
+
+        containerPassword.addView(etPassword, lp2);
+
+        linePassword = new View(getContext());
+        linePassword.setId(R.id.login_line_password);
+        linePassword.setBackgroundColor(ResTools.getColor(R.color.c1));
+
+        RelativeLayout.LayoutParams lp5 =
+                new RelativeLayout.LayoutParams(LayoutParams.MATCH_PARENT, ResTools.getDimenInt(R.dimen.common_line_height));
+        lp5.addRule(RelativeLayout.BELOW, R.id.login_et_password);
+
+        containerPassword.addView(linePassword, lp5);
+
+        mContainer.addView(containerPassword, lp4);
+
+        // containerOKButton
+
+        containerOKButton = new RelativeLayout(getContext());
+        containerOKButton.setId(R.id.login_rl_button);
+
+        RelativeLayout.LayoutParams lp6 = new RelativeLayout.LayoutParams(ResTools.getDimenInt(R.dimen.login_rl_button_width_height),
+                ResTools.getDimenInt(R.dimen.login_rl_button_width_height));
+        lp6.topMargin = ResTools.getDimenInt(R.dimen.login_rl_button_margin_top);
+        lp6.addRule(RelativeLayout.BELOW, R.id.login_rl_password);
+        lp6.addRule(RelativeLayout.CENTER_HORIZONTAL);
+
+        okButton = new CircleView(getContext(), ResTools.getDimenInt(R.dimen.login_bt_radius),
+                ResTools.getDimenInt(R.dimen.login_bt_radius), ResTools.getDimenInt(R.dimen.login_bt_radius));
+        okButton.setColor(ResTools.getColor(R.color.c1));
+        okButton.setAlpha(0.3f);
+
+        containerOKButton.addView(okButton);
+
+        okButtonArrow = new ImageView(getContext());
+        okButtonArrow.setImageDrawable(ResTools.getDrawable(R.drawable.arrow_right));
+
+        RelativeLayout.LayoutParams lp7 = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        lp7.addRule(RelativeLayout.CENTER_IN_PARENT);
+
+        containerOKButton.addView(okButtonArrow, lp7);
+
+        mContainer.addView(containerOKButton, lp6);
 
 
     }
---
+++ app/src/main/java/com/dv/persistnote/framework/ui/CircleView.java
@@ -0,0 +1,49 @@
+package com.dv.persistnote.framework.ui;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.View;
+
+/**
+ * Created by Admin2 on 2016/4/1.
+ */
+public class CircleView extends View {
+    private float m_cx;
+    private float m_cy;
+    private float m_radius;
+    private Paint paint;
+
+    public CircleView(Context context, float cx, float cy, float radius) {
+        super(context);
+        m_cx = cx;
+        m_cy = cy;
+        m_radius = radius;
+
+        // 首先定义一个paint
+        paint = new Paint();
+
+        // 绘制矩形区域-实心矩形
+        // 设置颜色
+        paint.setColor(Color.BLUE);
+        // 设置样式-填充
+        paint.setStyle(Paint.Style.FILL);
+    }
+
+    public void setColor(int color) {
+        // 设置颜色
+        paint.setColor(color);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        // TODO Auto-generated method stub
+        super.onDraw(canvas);
+
+        // draw circle
+        canvas.drawCircle(m_cx, m_cy, m_radius, paint);
+    }
+
+
+}
