54770a1a11c0aa9cdee1b311da6505d6a4d9bbe1
cmcchemc <cmcchemc@gmail.com>
2016-04-16 04:10:47 +0800

    Check and add ModelId

--- app/src/main/java/com/dv/persistnote/framework/model/ModelId.java
+++ app/src/main/java/com/dv/persistnote/framework/model/ModelId.java
@@ -7,4 +7,5 @@ public class ModelId {
     public static final int OnHabitListLoaded = 1001;
     public static final int OnCommunityLoaded = 1002;
     public static final int OnCommunityNoMore = 1003;
+    public static final int OnCommunityRefresh = 1004;
 }
