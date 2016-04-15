53936e9a594f9004436dd9fc35db6d370c9a0d26
cmcchemc <cmcchemc@gmail.com>
2016-04-16 02:53:48 +0800

    link Glide and CommunityList support load more

---
+++ app/src/main/java/com/dv/persistnote/base/network/DomainConfig.java
@@ -0,0 +1,8 @@
+package com.dv.persistnote.base.network;
+
+/**
+ * Created by Hang on 2016/4/14.
+ */
+public class DomainConfig {
+    public static String MAIN_DOMAIN = "http://news-at.zhihu.com";
+}
