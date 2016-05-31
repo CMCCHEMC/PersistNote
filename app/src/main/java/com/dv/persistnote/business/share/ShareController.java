4fb8bbcaa0942178d8ed8083a0946bee6afa3528
orange90 <just@setmefree.cn>
2016-06-01 00:32:29 +0800

    接入分享代码

--- app/src/main/java/com/dv/persistnote/business/share/ShareController.java
+++ app/src/main/java/com/dv/persistnote/business/share/ShareController.java
@@ -1,5 +1,6 @@
 package com.dv.persistnote.business.share;
 
+import android.content.Context;
 import android.os.Message;
 import android.widget.Toast;
 
@@ -7,22 +8,45 @@ import com.dv.persistnote.framework.core.AbstractController;
 import com.dv.persistnote.framework.core.BaseEnv;
 import com.dv.persistnote.framework.core.MsgDef;
 import com.dv.persistnote.framework.ui.AbstractScreen;
-
+import cn.sharesdk.onekeyshare.OnekeyShare;
+import cn.sharesdk.onekeyshare.OnekeyShareTheme;
 /**
  * Created by Hang on 2016/5/20.
  */
 public class ShareController  extends AbstractController {
-
+    public static String WEB_SITE = "http://yinfan.org";
     public ShareController(BaseEnv baseEnv) {
         super(baseEnv);
     }
+    private ShareData m_shareData;
 
     @Override
     public void handleMessage(Message msg) {
         if(msg.what == MsgDef.MSG_OPEN_SHARE_PLATFORM) {
-            ShareData shareData = (ShareData)msg.obj;
-            Toast.makeText(mContext, "菊花分享接这里\n"+shareData.mTitle, Toast.LENGTH_SHORT).show();
+            m_shareData = (ShareData) msg.obj;
+            showShare(this.mContext,null,true);
+           // Toast.makeText(mContext, "菊花分享接这里\n"+shareData.mTitle, Toast.LENGTH_SHORT).show();
         }
+    }
+
+    public void showShare(Context context, String platformToShare, boolean showContentEdit) {
+        OnekeyShare oks = new OnekeyShare();
+        oks.setSilent(!showContentEdit);
+        if (platformToShare != null) {
+            oks.setPlatform(platformToShare);
+        }
+        //ShareSDK快捷分享提供两个界面第一个是九宫格 CLASSIC  第二个是SKYBLUE
+        oks.setTheme(OnekeyShareTheme.CLASSIC);
+        // 令编辑页面显示为Dialog模式
+        oks.setDialogMode();
+        // 在自动授权时可以禁用SSO方式
+        oks.disableSSOWhenAuthorize();
+        //oks.setAddress("12345678901"); //分享短信的号码和邮件的地址
+        oks.setTitle(m_shareData.mTitle);
+        oks.setTitleUrl(m_shareData.mImageUrl);
+        oks.setText(m_shareData.mContent);
+        //oks.setImageUrl(m_shareData.mImageUrl); //微信不绕过审核分享链接
+        oks.show(context);
     }
 
     @Override
